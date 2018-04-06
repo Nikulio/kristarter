d5f6f65263f80962646c97cafcca1cf632c4e72a
Nikita Shchypylov <sn97@icloud.com>
2018-04-06 14:37:06 +0300

    cleaning up

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,6 @@
 import * as consts from './consts';
 
 export const initAction = (data) => {
-  console.log(data);
-  console.log(consts.INIT_ACTION);
   return {
     type: consts.INIT_ACTION,
     payload: data
